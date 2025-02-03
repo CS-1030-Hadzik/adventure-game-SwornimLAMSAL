08953f25530c705749d3c68660b2aaaff5f6fd8b
Swornim <swornimlamsal@mail.weber.edu>
2025-02-02 21:53:00 -0700

    Asking the players name

--- game.py
+++ game.py
@@ -1,3 +1,17 @@
+"""
+Adventure Game
+Author: Swornim Lamsal
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+"""
+
+
 # Welcome message and introduction
 print("welcome to the adventure game!")
 print("your journey begins here ......")
+
+
+# Ask for the player's name
+player_name = input("What is your name, adventurer? ")
